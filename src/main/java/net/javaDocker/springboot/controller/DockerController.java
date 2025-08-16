c3a0ba2e96acba271c19d950cebe5f83788eff09
pradeep <pradeep.tkreddy@gmail.com>
2025-08-16 16:53:49 -0400

    added comment

--- src/main/java/net/javaDocker/springboot/controller/DockerController.java
+++ src/main/java/net/javaDocker/springboot/controller/DockerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DockerController {
     
+    // getmapping annataion	
     @GetMapping("/docker")
     public String dockerDemo(){
 	//this is returning message while calling get request
